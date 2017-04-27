6711bdff1fb5d4aa03a6fbb99662d2d490e8da56
ericholguin <eric.holguinsanchez@gmail.com>
2017-04-26 23:55:48 -0600

    Fixed tab indexing

--- main.py
+++ main.py
@@ -43,95 +43,95 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
             elif (self.Click6.isSelected() == True):
                  self.tabWidget.addTab(self.braaTab, "braa")
             elif (self.Click7.isSelected() == True):
-                 self.tabWidget.addTab(self.braaTab, "braa")
-            elif (self.Click8.isSelected() == True):
                  self.tabWidget.addTab(self.CaseFileTab, "CaseFile")
-            elif (self.Click9.isSelected() == True):
+            elif (self.Click8.isSelected() == True):
                  self.tabWidget.addTab(self.CDPSnarfTab, "CDPSnarf")
-            elif (self.Click10.isSelected() == True):
+            elif (self.Click9.isSelected() == True):
                  self.tabWidget.addTab(self.ciscoTorchTab, "cisco-torch")
-            elif (self.Click11.isSelected() == True):
+            elif (self.Click10.isSelected() == True):
                  self.tabWidget.addTab(self.CookieCadgerTab, "Cookie Cadger")
-            elif (self.Click12.isSelected() == True):
+            elif (self.Click11.isSelected() == True):
                  self.tabWidget.addTab(self.copyRouterConfigTab, "copy-router-config")
-            elif (self.Click13.isSelected() == True):
+            elif (self.Click12.isSelected() == True):
                  self.tabWidget.addTab(self.DMitryTab, "DMitry")
-            elif (self.Click14.isSelected() == True):
+            elif (self.Click13.isSelected() == True):
                  self.tabWidget.addTab(self.dnmapTab, "dnmap")
-            elif (self.Click15.isSelected() == True):
+            elif (self.Click14.isSelected() == True):
                  self.tabWidget.addTab(self.dnsenumTab, "dnsenum")
-            elif (self.Click16.isSelected() == True):
+            elif (self.Click15.isSelected() == True):
                  self.tabWidget.addTab(self.dnsmapTab, "dnsmap")
-            elif (self.Click17.isSelected() == True):
+            elif (self.Click16.isSelected() == True):
                  self.tabWidget.addTab(self.DNSReconTab, "DNSRecon")
-            elif (self.Click18.isSelected() == True):
+            elif (self.Click17.isSelected() == True):
                  self.tabWidget.addTab(self.dnstracerTab, "dnstracer")
-            elif (self.Click19.isSelected() == True):
+            elif (self.Click18.isSelected() == True):
                  self.tabWidget.addTab(self.dnswalkTab, "dnswalk")
-            elif (self.Click20.isSelected() == True):
+            elif (self.Click19.isSelected() == True):
                  self.tabWidget.addTab(self.DotDotPwnTab, "DotDotPwn")
-            elif (self.Click21.isSelected() == True):
+            elif (self.Click20.isSelected() == True):
                  self.tabWidget.addTab(self.enum4linuxTab, "enum4linux")
-            elif (self.Click22.isSelected() == True):
+            elif (self.Click21.isSelected() == True):
                  self.tabWidget.addTab(self.enumIAXTab, "enumIAX")
-            elif (self.Click23.isSelected() == True):
+            elif (self.Click22.isSelected() == True):
                  self.tabWidget.addTab(self.Faraday, "Faraday")
-            elif (self.Click24.isSelected() == True):
+            elif (self.Click23.isSelected() == True):
                  self.tabWidget.addTab(self.FierceTab, "Fierce")
-            elif (self.Click25.isSelected() == True):
+            elif (self.Click24.isSelected() == True):
                  self.tabWidget.addTab(self.FireWalkTab, "Firewalk")
-            elif (self.Click26.isSelected() == True):
+            elif (self.Click25.isSelected() == True):
                  self.tabWidget.addTab(self.fragRouteTab, "fragroute")
-            elif (self.Click27.isSelected() == True):
+            elif (self.Click26.isSelected() == True):
                  self.tabWidget.addTab(self.fragRouterTab, "fragrouter")
-            elif (self.Click28.isSelected() == True):
+            elif (self.Click27.isSelected() == True):
                  self.tabWidget.addTab(self.GhostPhisherTab, "Ghost Phisher")
-            elif (self.Click29.isSelected() == True):
+            elif (self.Click28.isSelected() == True):
                  self.tabWidget.addTab(self.GoLismeroTab, "GoLismero")
-            elif (self.Click30.isSelected() == True):
+            elif (self.Click29.isSelected() == True):
                  self.tabWidget.addTab(self.goofileTab, "goofile")
-            elif (self.Click31.isSelected() == True):
+            elif (self.Click30.isSelected() == True):
                  self.tabWidget.addTab(self.hping3Tab, "hping3")
-            elif (self.Click32.isSelected() == True):
+            elif (self.Click31.isSelected() == True):
                  self.tabWidget.addTab(self.identUserEnumTab, "ident-user-enum")
-            elif (self.Click33.isSelected() == True):
+            elif (self.Click32.isSelected() == True):
                  self.tabWidget.addTab(self.InTraceTab, "InTrace")
-            elif (self.Click34.isSelected() == True):
+            elif (self.Click33.isSelected() == True):
                  self.tabWidget.addTab(self.iSMTPTab, "iSMTP")
-            elif (self.Click35.isSelected() == True):
+            elif (self.Click34.isSelected() == True):
                  self.tabWidget.addTab(self.IbdTab, "Ibd")
-            elif (self.Click36.isSelected() == True):
+            elif (self.Click35.isSelected() == True):
                  self.tabWidget.addTab(self.MaltegoTeethTab, "Maltego Teeth")
-            elif (self.Click37.isSelected() == True):
+            elif (self.Click36.isSelected() == True):
                  self.tabWidget.addTab(self.masscanTab, "masscan")
-            elif (self.Click38.isSelected() == True):
+            elif (self.Click37.isSelected() == True):
                  self.tabWidget.addTab(self.MetagoofilTab, "Metagoofil")
-            elif (self.Click39.isSelected() == True):
+            elif (self.Click38.isSelected() == True):
                  self.tabWidget.addTab(self.MirandaTab, "Miranda")
-            elif (self.Click40.isSelected() == True):
+            elif (self.Click39.isSelected() == True):
                  self.tabWidget.addTab(self.nbtscanUnixwizTab, "nbtscan-unix")
-            elif (self.Click41.isSelected() == True):
+            elif (self.Click40.isSelected() == True):
                  self.tabWidget.addTab(self.NmapTab, "Nmap")
-            elif (self.Click42.isSelected() == True):
+            elif (self.Click41.isSelected() == True):
                  self.tabWidget.addTab(self.ntopTab, "ntop")
-            elif (self.Click43.isSelected() == True):
+            elif (self.Click42.isSelected() == True):
                  self.tabWidget.addTab(self.p0fTab, "p0f")
-            elif (self.Click44.isSelected() == True):
+            elif (self.Click43.isSelected() == True):
                  self.tabWidget.addTab(self.PareseroTab, "Parsero")
-            elif (self.Click45.isSelected() == True):
+            elif (self.Click44.isSelected() == True):
                  self.tabWidget.addTab(self.ReconNgTab, "Recon-ng")
-            elif (self.Click46.isSelected() == True):
+            elif (self.Click45.isSelected() == True):
                  self.tabWidget.addTab(self.SETTab, "SET")
-            elif (self.Click47.isSelected() == True):
+            elif (self.Click46.isSelected() == True):
                  self.tabWidget.addTab(self.smtpUserEnumTab, "smtp-user-enum")
-            elif (self.Click48.isSelected() == True):
+            elif (self.Click47.isSelected() == True):
                  self.tabWidget.addTab(self.snmpChecktab, "snmp-check")
-            elif (self.Click49.isSelected() == True):
+            elif (self.Click48.isSelected() == True):
                  self.tabWidget.addTab(self.spartaTab, "SPARTA")
-            elif (self.Click50.isSelected() == True):
+            elif (self.Click49.isSelected() == True):
                  self.tabWidget.addTab(self.sscaudittab, "sslcaudit")
-            elif (self.Click51.isSelected() == True):
+            elif (self.Click50.isSelected() == True):
                  self.tabWidget.addTab(self.SSLsplitab, "SSLsplit")
+            elif (self.Click51.isSelected() == True):
+                 self.tabWidget.addTab(self.sslstripTab, "sslstrip")
             elif (self.Click52.isSelected() == True):
                  self.tabWidget.addTab(self.SSLyzeTab, "SSLyze")
             elif (self.Click53.isSelected() == True):
@@ -152,248 +152,6 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
                  self.tabWidget.addTab(self.XplicoTab, "Xplico")
 
         self.treeWidget.itemClicked.connect(openTab)
-
-        # if (self.Click1.isSelected() == True):
-        #     {
-        #         self.treeWidget.itemClicked(self.Click1, 0).connect(openTab)
-        #         #self.connect(self.tabWidget.addTab(self.acccheckTab, "acccheck")
-        #     }
-        # if (self.Click2.isSelected()):
-        #     {
-        #         self.tabWidget.addTab(self.acevoipTab, "ace-voip")
-        #     }
-        # if (self.Click3.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.amapTab, "amap")
-        #     }
-        # if (self.Click4.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.automaterTab, "Automater")
-        #     }
-        # if (self.Click5.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.bingip2hostsTab, "bing-ip2hosts")
-        #     }
-        # if (self.Click6.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.braaTab, "braa")
-        #     }
-        # if (self.Click7.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.CaseFileTab, "CaseFile")
-        #     }
-        # if (self.Click8.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.CDPSnarfTab, "CDPSnarf")
-        #     }
-        # if (self.Click9.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.ciscoTorchTab, "cisco-torch")
-        #     }
-        # if (self.Click10.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.CookieCadgerTab, "Cookie Cadger")
-        #     }
-        # if (self.Click11.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.copyRouterConfigTab, "copy-router-config")
-        #     }
-        # if (self.Click12.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.DMitryTab, "DMitry")
-        #     }
-        # if (self.Click13.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.dnmapTab, "dnmap")
-        #     }
-        # if (self.Click14.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.dnsenumTab, "dnsenum")
-        #     }
-        # if (self.Click15.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.dnsmapTab, "dnsmap")
-        #     }
-        # if (self.Click16.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.DNSReconTab, "DNSRecon")
-        #     }
-        # if (self.Click17.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.dnstracerTab, "dnstracer")
-        #     }
-        # if (self.Click18.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.dnswalkTab, "dnswalk")
-        #     }
-        # if (self.Click19.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.DotDotPwnTab, "DotDotPwn")
-        #     }
-        # if (self.Click20.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.enum4linuxTab, "enum4linux")
-        #     }
-        # if (self.Click21.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.enumIAXTab, "enumIAX")
-        #     }
-        # if (self.Click22.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.Faraday, "Faraday")
-        #     }
-        # if (self.Click23.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.FierceTab, "Fierce")
-        #     }
-        # if (self.Click24.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.FireWalkTab, "Firewalk")
-        #     }
-        # if (self.Click25.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.fragRouteTab, "fragroute")
-        #     }
-        # if (self.Click26.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.fragRouterTab, "fragrouter")
-        #     }
-        # if (self.Click27.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.GhostPhisherTab, "Ghost Phisher")
-        #     }
-        # if (self.Click28.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.GoLismeroTab, "GoLismero")
-        #     }
-        # if (self.Click29.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.goofileTab, "goofile")
-        #     }
-        # if (self.Click30.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.hping3Tab, "hping3")
-        #     }
-        # if (self.Click31.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.identUserEnumTab, "ident-user-enum")
-        #     }
-        # if (self.Click32.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.InTraceTab, "InTrace")
-        #     }
-        # if (self.Click33.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.iSMTPTab, "iSMTP")
-        #     }
-        # if (self.Click34.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.IbdTab, "Ibd")
-        #     }
-        # if (self.Click35.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.MaltegoTeethTab, "Maltego Teeth")
-        #     }
-        # if (self.Click36.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.masscanTab, "masscan")
-        #     }
-        # if (self.Click37.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.MetagoofilTab, "Metagoofil")
-        #     }
-        # if (self.Click38.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.MirandaTab, "Miranda")
-        #     }
-        # if (self.Click39.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.nbtscanUnixwizTab, "nbtscan-unix")
-        #     }
-        # if (self.Click40.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.NmapTab, "Nmap")
-        #     }
-        # if (self.Click41.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.ntopTab, "ntop")
-        #     }
-        # if (self.Click42.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.p0fTab, "p0f")
-        #     }
-        # if (self.Click43.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.PareseroTab, "Parsero")
-        #     }
-        # if (self.Click44.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.ReconNgTab, "Recon-ng")
-        #     }
-        # if (self.Click45.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.SETTab, "SET")
-        #     }
-        # if (self.Click46.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.smtpUserEnumTab, "smtp-user-enum")
-        #     }
-        # if (self.Click47.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.snmpChecktab, "snmp-check")
-        #     }
-        # if (self.Click48.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.spartaTab, "SPARTA")
-        #     }
-        # if (self.Click49.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.sscaudittab, "sslcaudit")
-        #     }
-        # if (self.Click50.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.SSLsplitab, "SSLsplit")
-        #     }
-        # if (self.Click51.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.sslstripTab, "sslstrip")
-        #     }
-        # if (self.Click52.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.SSLyzeTab, "SSLyze")
-        #     }
-        # if (self.Click53.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.THCIPV6Tab, "THC-IPV6")
-        #     }
-        # if (self.Click54.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.theHarvesterTab, "theHarvester")
-        #     }
-        # if (self.Click55.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.TLSSLedTab, "TLSSLed")
-        #     }
-        # if (self.Click56.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.twofiTab, "twofi")
-        #     }
-        # if (self.Click57.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.urlCrazyTab, "UrlCrazy")
-        #     }
-        # if (self.Click58.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.WiresharkTab, "Wireshark")
-        #     }
-        # if (self.Click59.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.WOLETab, "WOL-E")
-        #     }
-        # if (self.Click60.isSelected):
-        #     {
-        #         self.tabWidget.addTab(self.XplicoTab, "Xplico")
-        #    }
 
 
 def main():
